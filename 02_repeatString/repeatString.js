602a579a149e3ec0a747b8d8a527d1b198c6cb91
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-05 17:26:29 +0100

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let arr = [];
 
+    for(i = 0; i < num; i++){
+       arr.push(string); 
+    }
+
+    let str = arr.join("");
+
+    return str;
 };
 
 // Do not edit below this line

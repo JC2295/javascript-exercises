5331b07d057356a08dca90b0a490e24000e04431
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-05 18:32:26 +0100

    Completed Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let tot = 0;
+
+    if(num1 > num2){
+
+        for(let i = num2; i <= num1; i++){
+            tot += i;
+        }
+
+        return tot;
+    }
+    else if(num2 > num1){
+
+        for(let i = num1; i <= num2; i++){
+            tot += i;
+        }
+
+        return tot;
+
+    }
+    else{
+        return num1 + num2;
+    }
 
 };
 

e7aea6982dacbab5dcd069964629d35e687432c4
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-06 08:40:24 +0100

    Completed exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = ((fTemp - 32) * 5 )/ 9;
+  
+  cTemp.toFixed(1);
+
+  if(cTemp == 0.0){
+    return 0;
+  }
+  else{
+    return cTemp;
+  }
+
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+
+  let fTemp = ((cTemp * 9) / 5) + 32;
+  //console.log(fahrenheit);
+  fTemp.toFixed(1);
+
+  if(fTemp == 0.0){
+    return 0;
+  }
+  else{
+    return fTemp;
+  }
+  
 };
 
 // Do not edit below this line
